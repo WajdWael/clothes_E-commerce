b1234c26fb72f16844760153b319cb67c48569c2
unknown <wajdabdalhay@gmail.com>
2022-03-09 15:28:26 +0300

    edit4

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,3 @@
-// ==== Swiper (Home section) ====
-var homeSwiper = new Swiper(".home-swiper", {
-    spaceBetween: 30,
-    loop: 'true',
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
-
-// ==== Change Backgorund Header ====
-function scrollHeader() {
-    const header = document.getElementById('header');
-    // when the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 50) header.classList.add('scroll-header');
-    else header.classList.remove('scroll-header');
-}
-window.addEventListener('scroll', scrollHeader);
-
-// ==== Swiper (New section) ====
-var newSwiper = new Swiper(".new-swiper", {
-    spaceBetween: 16,
-    centeredSlides: true,
-    slidesPerView: 'auto',
-    loop: 'true',
-});
-
-
-
 // ==== Shwo menu ====
 const navMenu = document.querySelector('#nav-menu'),
     navToggleBtn = document.querySelector('.nav__toggle'),
